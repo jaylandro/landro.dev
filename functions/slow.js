7a0048da996f5bc6b7a3d43c37f0f2c563dfa9f7
Jay Landro <jaylandro@gmail.com>
2021-02-27 16:15:05 -0800

    Added simulated slow api function

---
+++ functions/slow.js
@@ -0,0 +1,25 @@
+exports.handler = async function (event, context) {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    const response = await fetch("https://api.github.com/users?per_page=5", {
+      Accept: "application/json",
+    });
+
+    if (!response.ok)
+      return { statusCode: response.status, body: response.statusText };
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }),
+    };
+  }
+};
